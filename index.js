f016d317ca3cf5d0a5a710db180e31b94610d90e
Caih2023 <estadiacaih@gmail.com>
2023-06-06 13:38:22 -0600

    add files

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 
 // Rutas
 app.get("/", (req, res) => {
-    res.send("API REST Estadias CAIH");
+    res.send("API PARA CAIH");
 });
 
 app.use('/api', usuariosRuta);
